b45b38e52d2e492b17efcd45041f6cfeb6200813
Kiera-Hegarty <98025303+Kiera-Hegarty@users.noreply.github.com>
2022-03-04 15:41:41 +0000

    Adding MySql file

---
+++ src/main/resources/sea_creature.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE sea_creature;
+USE sea_creature;
+
+create table sea_creature (id integer AUTO_INCREMENT,dorsal_fin boolean, life_expectancy integer, mammal boolean, name varchar(255) not null, primary key (id));
+
+SELECT * FROM sea_creature;
