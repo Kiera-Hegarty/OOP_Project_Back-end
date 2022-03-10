1436442bfeef67ab62c25d2df2c514b9d21d39cb
Kiera-Hegarty <98025303+Kiera-Hegarty@users.noreply.github.com>
2022-03-10 18:07:47 +0000

    Update SeaCreatureController.java

--- src/main/java/com/qa/seacreature/web/SeaCreatureController.java
+++ src/main/java/com/qa/seacreature/web/SeaCreatureController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.qa.seacreature.Service.SeaCreatureService;
 import com.qa.seacreature.domain.SeaCreature;
 
+@CrossOrigin 
 @RestController
 public class SeaCreatureController {
 	
