4f44b51bfc1245f92bb29770f84c06cd3896507a
Kiera-Hegarty <98025303+Kiera-Hegarty@users.noreply.github.com>
2022-03-10 17:49:11 +0000

    Update sea_creature.sql

--- src/main/resources/sea_creature.sql
+++ src/main/resources/sea_creature.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE sea_creature;
 USE sea_creature;
 
-create table sea_creature (id integer AUTO_INCREMENT,dorsal_fin boolean, life_expectancy integer, mammal boolean, name varchar(255) not null, primary key (id));
+create table sea_creature (id integer AUTO_INCREMENT,dorsal_fin boolean, life_expectancy integer, mammal boolean, name varchar(255), primary key (id));
 
 SELECT * FROM sea_creature;
